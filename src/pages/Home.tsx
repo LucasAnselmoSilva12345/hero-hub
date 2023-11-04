07b6d7376bdf5a455ac62866a3cb302f0cdca5bc
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-11-04 14:40:24 -0300

    style: Homepage title
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { SuperheroList } from '../components/SuperheroList';
 export function Home() {
   return (
     <section className="p-4">
-      <h2>Super-heroes list</h2>
+      <h2 className="text-xl text-gray-200 font-medium">Super-heroes list</h2>
 
       <SuperheroList />
     </section>
