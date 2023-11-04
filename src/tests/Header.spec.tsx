ab262bfe7704a00660ca0e05700e146c09472deb
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-11-04 15:36:59 -0300

    Test: Refactor unit test of Header Component
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/tests/Header.spec.tsx
+++ src/tests/Header.spec.tsx
@@ -5,6 +5,14 @@ describe('Header', () => {
   it('should show the name of website', () => {
     render(<RoutesApp />);
 
-    expect(screen.getByText(/Hero-Hub/i)).toBeInTheDocument();
+    const homeTitle = screen.getByText(/Hero-Hub/i);
+    expect(homeTitle).toBeInTheDocument();
+  });
+
+  it('should header links have the correct href', () => {
+    render(<RoutesApp />);
+
+    const cardsLink = screen.getByText('Cards');
+    expect(cardsLink).toHaveAttribute('href', '/');
   });
 });
