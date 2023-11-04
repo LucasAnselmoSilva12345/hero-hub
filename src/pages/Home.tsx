c94e64f8774327dd720750cc5a94e40b44ddf5dd
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-11-04 12:47:39 -0300

    Feat: import SuperHeroList component
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,11 @@
+import { SuperheroList } from '../components/SuperheroList';
+
 export function Home() {
   return (
     <section className="p-4">
       <h2>Super-heroes list</h2>
+
+      <SuperheroList />
     </section>
   );
 }
