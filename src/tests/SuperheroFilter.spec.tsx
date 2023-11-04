0e80771a2efa15903542af514fea1e0e69f35494
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-11-04 15:07:03 -0300

    test: SuperheroFilter component
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

---
+++ src/tests/SuperheroFilter.spec.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { screen, render } from '@testing-library/react';
+import { SuperheroFilter } from '../components/SuperheroFilter';
+
+describe('Superhero Filter', () => {
+  it('should show filter placeholder on superhero input', async () => {
+    render(
+      <SuperheroFilter
+        filter={''}
+        onFilterChange={function (filter: string): void {
+          throw new Error('Function not implemented.');
+        }}
+      />
+    );
+
+    const input = screen.getByPlaceholderText(/Search by superhero.../i);
+    expect(input).toBeInTheDocument();
+  });
+});
