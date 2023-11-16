e5f019e7d9e56df2ee579eed348fdf172293d827
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-11-16 16:46:26 -0300

    feat: Apply styles to each superhero card
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/SuperheroList.tsx
+++ src/components/SuperheroList.tsx
@@ -17,8 +17,6 @@ interface SuperheroProps {
     race: string;
   };
   biography: {
-    fullName: string;
-    alterEgos: string;
     firstAppearance: string;
     publisher: string;
   };
@@ -57,34 +55,76 @@ export function SuperheroList() {
   return (
     <div className="py-4 text-center lg:text-right">
       <SuperheroFilter filter={filter} onFilterChange={setFilter} />
-      <ul className="py-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5">
+      <ul className="py-4 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         {filteredSuperheroes.map((superhero) => (
           <li
             key={superhero.id}
-            className="p-2 text-center cursor-pointer transition-all duration-200 hover:scale-105"
+            className="w-full text-center border rounded-lg border-zinc-800 transition-all duration-200 hover:border-blue-950"
           >
-            <img
-              src={superhero.images.sm}
-              alt={superhero.name}
-              className="mx-auto my-0 p-1 border"
-            />
-            <h2 className="font-medium text-gray-200">{superhero.name}</h2>
-            <div>
-              <ul>
-                <li>{superhero.powerstats.intelligence}</li>
-                <li>{superhero.powerstats.strength}</li>
-                <li>{superhero.powerstats.speed}</li>
-                <li>{superhero.powerstats.durability}</li>
-                <li>{superhero.powerstats.power}</li>
-                <li>{superhero.powerstats.combat}</li>
+            <div className="flex items-center justify-between">
+              <img
+                src={superhero.images.sm}
+                alt={superhero.name}
+                className=""
+              />
+              <div className="flex flex-1 flex-col">
+                <h2 className="text-zinc-200 text-4xl font-bold">
+                  {superhero.name}
+                </h2>
+                <h3 className="text-zinc-600 text-base font-medium">
+                  {superhero.appearance.race}
+                </h3>
+              </div>
+            </div>
+
+            <div className="py-2">
+              <ul className="grid grid-cols-1 px-3">
+                <li className="flex items-center justify-between">
+                  <p className="mr-1 font-medium">Intelligence</p>
+                  <hr className="flex-grow border-b border-dotted border-b-zinc-700 mx-0 my-[.625rem]" />
+                  <span className="ml-1 font-light">
+                    {superhero.powerstats.intelligence}
+                  </span>
+                </li>
+                <li className="flex items-center justify-between">
+                  <p className="font-medium">Strength</p>
+                  <span className="font-light">
+                    {superhero.powerstats.strength}
+                  </span>
+                </li>
+                <li className="flex items-center justify-between">
+                  <p className="font-medium">Speed</p>
+                  <span className="font-light">
+                    {superhero.powerstats.speed}
+                  </span>
+                </li>
+                <li className="flex items-center justify-between">
+                  <p className="font-medium">Durability</p>
+                  <span className="font-light">
+                    {superhero.powerstats.durability}
+                  </span>
+                </li>
+                <li className="flex items-center justify-between">
+                  <p className="font-medium">Power</p>
+                  <span className="font-light">
+                    {superhero.powerstats.power}
+                  </span>
+                </li>
+                <li className="flex items-center justify-between">
+                  <p className="font-medium">Combat</p>
+                  <span className="font-light">
+                    {superhero.powerstats.combat}
+                  </span>
+                </li>
               </ul>
             </div>
 
-            <h4>{superhero.appearance.race}</h4>
-            <h4>{superhero.biography.fullName}</h4>
-            <h4>{superhero.biography.alterEgos}</h4>
-            <h4>{superhero.biography.firstAppearance}</h4>
-            <h4>{superhero.biography.publisher}</h4>
+            <div className="bg-blue-950 font-semibold flex items-center justify-between p-3 rounded-b-md">
+              <h6 className="text-sm">{superhero.biography.firstAppearance}</h6>
+              <span className="text-xs font-medium">
+                {superhero.biography.publisher}
+              </span>
+            </div>
           </li>
         ))}
       </ul>
